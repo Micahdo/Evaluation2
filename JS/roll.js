c347edc051660b352855fa0d4926b7920be23480
Micahdo <124348225+Micahdo@users.noreply.github.com>
2023-02-24 10:19:38 +0100

    Réorganisation du code

--- JS/roll.js
+++ JS/roll.js
@@ -14,67 +14,22 @@ let global2 = document.getElementById("global2");
 let diceImg = document.getElementById("dice-img");
 let activePlayer = 0;
 
-/*Nouveau jeu*/
-newButton.addEventListener("click", function() {
-    global1.innerHTML = 0;
-    global2.innerHTML = 0;
-    round1.innerHTML = 0;
-    round2.innerHTML = 0;
-    diceImage.setAttribute("src", "Images/0.png");
-    activePlayer = Math.floor(Math.random()*2)+1;
-    playerTurn();
-})
-
 /*Joueur actif*/
 function playerTurn() {
     if (activePlayer == 1) {
+        /*Initialisation du joueur 1 comme joueur actif*/
         activePlayer1.style.opacity = "1";
         activePlayer2.style.opacity = "0";
         player1.style.fontWeight = "600";
         player2.style.fontWeight = "300";
     } else {
+        /*Initialisation du joueur 2 comme joueur actif*/
         activePlayer1.style.opacity = "0";
         activePlayer2.style.opacity = "1";
         player1.style.fontWeight = "300";
         player2.style.fontWeight = "600";
     }
 }
-
-/*Jet du dé à 6 faces*/
-let result = 0;
-rollButton.addEventListener("click", function() {
-    animationDice();
-    result = Math.floor(Math.random()*6)+1;
-    diceImage.setAttribute("src", "Images/" + result + ".png");
-    if (result > 1 && activePlayer == 1) {
-        round1.innerHTML = result + Number(round1.innerText);
-    } else if (result == 1 && activePlayer == 1) {
-        round1.innerHTML = 0;
-        activePlayer = 2;
-        playerTurn();
-    } else if (result > 1 && activePlayer == 2) {
-        round2.innerHTML = result + Number(round2.innerText);
-    } else if (result == 1 && activePlayer == 2) {
-        round2.innerHTML = 0;
-        activePlayer = 1;
-        playerTurn();
-    }
-})
-
-/*Conservation du résultat*/
-holdButton.addEventListener("click", function() {
-    if (activePlayer == 1) {
-        global1.innerHTML = Number(global1.innerText) + Number(round1.innerText);
-        round1.innerHTML = 0;
-        activePlayer = 2;
-        playerTurn();
-    } else {
-        global2.innerHTML = Number(global2.innerText) + Number(round2.innerText);
-        round2.innerHTML = 0;
-        activePlayer = 1;
-        playerTurn();
-    }
-})
 
 /*Animation du dé*/
 function animationDice() {
@@ -83,3 +38,65 @@ function animationDice() {
         diceImage.classList.remove("animateDice")
     }, 1000)
 }
+
+/*Nouveau jeu*/
+newButton.addEventListener("click", function() {
+    /*Mise à 0 des boîtes round et global*/
+    global1.innerHTML = 0;
+    global2.innerHTML = 0;
+    round1.innerHTML = 0;
+    round2.innerHTML = 0;
+    /*Chargement de l'image du dé par défaut*/
+    diceImage.setAttribute("src", "Images/0.png");
+    /*Choix aléatoire du premier joueur*/
+    activePlayer = Math.floor(Math.random()*2)+1;
+    playerTurn();
+})
+
+/*Jet du dé à 6 faces*/
+let result = 0;
+rollButton.addEventListener("click", function() {
+    /*Lancement de l'animation du dé*/
+    animationDice();
+    /*Résultat aléatoire*/
+    result = Math.floor(Math.random()*6)+1;
+    /*Chargement de l'image du dé en fonction de sa valeur*/
+    diceImage.setAttribute("src", "Images/" + result + ".png");
+    /*Résultat si joueur 1 ou joueur 2 est actif*/
+    if (result > 1 && activePlayer == 1) {
+        round1.innerHTML = result + Number(round1.innerText);
+    } else if (result == 1 && activePlayer == 1) {
+        round1.innerHTML = 0;
+        /*Changement de joueur*/
+        activePlayer = 2;        
+        playerTurn();
+    } else if (result > 1 && activePlayer == 2) {
+        round2.innerHTML = result + Number(round2.innerText);
+    } else if (result == 1 && activePlayer == 2) {
+        round2.innerHTML = 0;
+        /*Changement de joueur*/
+        activePlayer = 1;        
+        playerTurn();
+    }
+})
+
+/*Conservation du résultat*/
+holdButton.addEventListener("click", function() {
+    if (activePlayer == 1) {
+        /*Le résultat de round1 passe dans global1*/
+        global1.innerHTML = Number(global1.innerText) + Number(round1.innerText);
+        /*Remise à 0 de round1*/
+        round1.innerHTML = 0;
+        /*Changement de joueur*/
+        activePlayer = 2;
+        playerTurn();
+    } else {
+        /*Le résultat de round2 passe dans global2*/
+        global2.innerHTML = Number(global2.innerText) + Number(round2.innerText);
+        /*Remise à 0 de round2*/
+        round2.innerHTML = 0;
+        /*Changement de joueur*/
+        activePlayer = 1;
+        playerTurn();
+    }
+})
